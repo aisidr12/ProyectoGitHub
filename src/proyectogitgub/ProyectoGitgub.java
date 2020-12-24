aa96baa67db225f12d8bc924f43a35bef0828f41
Arturo <art2dit2@hotmail.com>
2020-12-24 18:22:49 +0100

    Agregamos saludo a todos

---
+++ src/proyectogitgub/ProyectoGitgub.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogitgub;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProyectoGitgub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(saludo());
+    }
+    
+    private static String saludo(){
+        return "Hola a todos ";
+    }
+
+}
